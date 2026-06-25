412d82f3db34a8748ab34d88454d6db00543ebe3
teinei <34274536+teinei@users.noreply.github.com>
2026-06-25 21:23:50 +0800

    Create tailwind-sm.js

---
+++ js/tailwind-sm.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dark: 'crimson',
+        primary: 'red',
+        secondary: 'teal',
+      },
+      fontFamily: {
+        // 名字統一：MyCustomFont
+        sans: ['chanel', 'sans-serif'],
+      },
+    }
+  }
+}
