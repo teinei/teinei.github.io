52675b4ecdef0f87f193b16dc017fb68ecb72a12
teinei <34274536+teinei@users.noreply.github.com>
2026-06-25 18:32:59 +0800

    Create windtail-sm.js

---
+++ js/windtail-sm.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        dark: 'black',
+        primary: 'aqua',
+        secondary: 'red',
+      },
+      fontFamily: {
+        // 名字統一：MyCustomFont
+        sans: ['chanel', 'sans-serif'],
+      },
+    }
+  }
+}
